76a982b3b3d48eee5fb3269d16e034ea90037ac0
mohamed eid <meid368@gmail.com>
2021-08-08 16:33:23 +0200

    binary search tree

---
+++ main.py
@@ -0,0 +1,141 @@
+# main
+from Queue import Queue
+    
+
+class Node():
+    
+    def __init__(self, data):
+        self.data = data
+        self.right = None
+        self.left = None
+        
+class Tree():
+    
+    def __init__(self):
+        self.root = None
+        
+    def addHelper(self, temp, val):
+                    
+        if val > temp.data:
+            if temp.right is None:
+                temp.right = Node(val)
+            else:
+                self.addHelper(temp.right, val)
+        else:
+            if temp.left is None:
+                temp.left = Node(val)
+            else:
+                self.addHelper(temp.left, val)
+    
+    def add(self, val):
+        
+        if self.root is None:
+            self.root = Node(val)
+            return
+        else:
+            self.addHelper(self.root, val)
+            
+    def maxHelper(self, temp):
+        
+        if temp.right is None:
+            return temp.data
+        else:
+            return self.maxHelper(temp.right)
+            
+    def max(self):
+        
+        return self.maxHelper(self.root)
+    
+    def minHelper(self, temp):
+        if temp.left is None:
+            return temp.data
+        else:
+            return self.minHelper(temp.left)
+        
+        
+    def min(self):
+        
+        return self.minHelper(self.root)
+    
+    def getHeightHelper(self, temp):
+        
+        if temp == None:
+            return -1
+        
+        leftSubTree = self.getHeightHelper(temp.left)
+        rightSubTree = self.getHeightHelper(temp.right)        
+    
+        return max(leftSubTree, rightSubTree) + 1
+    
+    def getHeight(self):
+        
+        if self.root == None:
+            return -1
+        else:
+            return self.getHeightHelper(self.root)
+        
+    def display_level_order(self):
+        
+        if self.root == None:
+            return
+        
+        
+        queue = Queue()
+        
+        queue.enqueue(self.root)
+                
+        while not queue.isEmpty():
+            
+            current = queue.front_value()
+           
+            queue.dequeue()
+            
+            print(current.data)
+            
+            if current.left != None:
+                queue.enqueue(current.left)
+            if current.right != None:
+                queue.enqueue(current.right)
+                
+    def preorderHelper(self, temp):
+        
+        if temp == None:
+            return
+        
+        #preorder
+        print(temp.data)
+        
+        self.preorderHelper(temp.left)
+        
+        #inorder
+        # print(temp.data)
+        
+        self.preorderHelper(temp.right)
+        
+        #postorder
+        # print(temp.data)
+        
+                
+    def preorder(self):
+        
+        if self.root == None:
+            return
+        
+        return self.preorderHelper(self.root)
+        
+        
+                         
+
+
+tree = Tree()
+
+tree.add(15)
+tree.add(6)
+tree.add(20)
+tree.add(3)
+tree.add(9)
+tree.add(8)
+tree.add(25)
+
+
+tree.preorder()
